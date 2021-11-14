d8411cd98deadaa1c9200aba31f80676201a360f
Randriantsoa <randriantsoamatthieu@gmail.com>
2021-11-14 01:49:38 +0100

    Application Upload

---
+++ js/animation.js
@@ -0,0 +1,95 @@
+'use-strict';
+
+// Window dimensions (eseless)
+let intViewportHeight = window.innerHeight;
+let intViewportWidth = window.innerWidth;
+
+// Just for develepement purpose
+//console.log(intViewportWidth);
+//console.log(intViewportHeight);
+
+// List of images for emotes
+let emotes = [
+    "./medias/gea.png",
+    "./medias/ahaha.jpg",
+    "./medias/aymeric.png",
+    "./medias/patience.png",
+    "./medias/anglade.jpg"
+];
+
+// Audio source to be played
+const audio = new Audio('./medias/crabCrave.mp3');
+
+/* Generates the emotes imgs */
+const generate = () => {
+    let container = document.getElementById("emote-container");
+
+    // Reset all emotes
+    container.innerHTML = "";
+
+    for (let i = 0; i < counter.count; i++) {
+        
+        // Creates img emotes instances
+        const newChild = document.createElement("img");
+        newChild.className = "emote";
+        newChild.src = emotes[Math.floor(Math.random() * emotes.length)];
+        newChild.style.position = "absolute";
+
+        rpositionx = Math.floor(Math.random() * 100)
+        rpositiony = Math.floor(Math.random() * 100)
+        console.log(rpositionx, rpositiony);
+
+        newChild.style.left = `${rpositionx}%`;
+        newChild.style.top = `${rpositiony}%`;
+
+        // Add them to the emotes container div
+        container.appendChild(newChild);
+    }
+   
+}
+
+
+// Animation function with GSAP Library
+const animation = () => {
+
+    // Check wether an animation is already running
+    if (isTweening()) return;
+
+    // Create all the childNodes
+    let container = document.getElementById("emote-container");
+    generate();
+
+    // If empty stop the audio and abort
+    if (container.childNodes.length <= 0) {
+        audio.pause();
+        audio.currentTime = 0;
+        return ;
+    };
+    
+    // Launch the audio if not already playing
+    if (audio.paused) {
+        audio.play();
+    }
+    
+
+    // Create a timeline for animation
+    const EmoteTimeline = gsap.timeline({onComplete: animation});
+
+    EmoteTimeline
+        .from(".emote", {duration: 1, opacity: 0})
+        .to(".emote", {
+            duration: 8, 
+            x: "random(-300, 300)", 
+            y: "random(-300, 300)", 
+            z: "random(0, 1000)", 
+            rotation: "random(-360, 360)", 
+            ease: "linear"
+        }, "-=1")
+        .to(".emote", {duration: 1, opacity: 0}, "-=1");
+
+}
+
+// Simple array function to check wether the animation is running
+const isTweening = () => gsap.isTweening('.emote');
+
+
---
+++ js/functions.js
@@ -0,0 +1,64 @@
+'use-strict';
+
+class Count {
+    constructor(count){
+        this.count = count;
+    }
+
+    increment(count){
+        this.count += 1;
+    }
+
+    decrement(count){
+        if (this.count -= 1 < 0) {
+            this.count -= 1;
+        }
+    }
+
+    reset(){
+        this.count = 0;
+    }
+}
+
+let count;
+
+// Check if the counter is already set by localStorage
+if (localStorage.getItem('counter') === null) {
+    count = 0;
+} else {
+    count = JSON.parse(localStorage.getItem('counter'));
+}
+
+let counter = new Count(count)
+
+// Update the view
+const updateVue = (counter) => {
+    element = document.getElementById('counter');
+    element.innerHTML = `Number : ${counter.count}`
+    localStorage.setItem('counter', JSON.stringify(counter.count));
+}
+
+
+// Functions that update the counter value
+function onclickIncr(counter) {
+    counter.increment();
+    updateVue(counter);
+}
+
+function onclickDecr(counter) {
+    counter.decrement();
+    updateVue(counter);
+}
+
+function onclickReset(counter) {
+    counter.reset();
+    updateVue(counter);
+}
+
+
+
+
+
+
+// Event listener that automatically display the counter value by default right after the page is loaded.
+window.addEventListener('load', updateVue(counter));
